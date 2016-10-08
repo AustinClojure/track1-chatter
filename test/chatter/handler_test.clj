e5e081b92fd832b7ae444702d19f9e98a3e0d203
Chad Dollins <chad.dollins@livingsocial.com>
2016-10-08 03:29:59 -0500

    Updates to project to support test-patch-1 documentation updates

--- test/chatter/handler_test.clj
+++ test/chatter/handler_test.clj
@@ -1,13 +1,23 @@
 (ns chatter.handler-test
-  (:require [clojure.test :refer :all]
-            [ring.mock.request :as mock]
-            [chatter.handler :refer :all]))
+  (:use [kerodon.core]
+        [kerodon.test]
+        [clojure.test])
+  (:require [chatter.handler :refer [app]]))
 
 (deftest test-app
   (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))))
+    (-> (session app)
+        (visit "/")
+		(has (status? 200) "page is found")
+		(has (some-text? "Our Chat App"))
+		(fill-in [:input.form-control] "Hooman")
+		(fill-in [:input.form-control] "Greetings")
+		(press [:input.btn.btn-primary])
+		(follow-redirect)
+		(has (status? 200) "message submitted successfully")
+		(has (some-text? "Greetings"))))
 
   (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+    (-> (session app)
+        (visit "/invalid")
+		(has (status? 404) "page not found"))))
